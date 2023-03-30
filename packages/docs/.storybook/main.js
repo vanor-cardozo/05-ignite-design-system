5d1838292527d7846eeb93c99a496cdb181249b7
vanor-cardozo <vanor.fc@gmail.com>
2023-03-30 10:45:17 -0300

    fix: Adiciona o subdominio para o Storybook em produção

--- packages/docs/.storybook/main.js
+++ packages/docs/.storybook/main.js
@@ -16,5 +16,12 @@ module.exports = {
   },
   "features": {
     "storyStoreV7": true
+  },
+  viteFinal: (config, { configType }) => {
+    if(configType === 'PRODUCTION') {
+      config.base = '/05-ignite-design-system/'
+    }
+
+    return config
   }
 }
